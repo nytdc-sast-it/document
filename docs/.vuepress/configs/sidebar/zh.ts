fa96234250a4092a243fb59690ab417ba486c5da
Taoning Ge <gtn1024o@outlook.com>
2021-12-19 18:31:41 +0800

    inrto-how-to-install-java

--- docs/.vuepress/configs/sidebar/zh.ts
+++ docs/.vuepress/configs/sidebar/zh.ts
@@ -1,5 +1,5 @@
 import type { SidebarConfig } from "@vuepress/theme-default";
 
 export const zh: SidebarConfig = {
-  "/java/": [{ text: "开始之前", children: ["intro-introduction-of-java.md"] }],
+  "/java/": [{ text: "开始之前", children: ["intro-introduction-of-java.md", "intro-how-to-install-java.md"] }],
 };
