218ed8ae1a8cbefc222c819ae6c81beb0980dc33
Taoning Ge <gtn1024o@outlook.com>
2022-05-22 22:40:46 +0800

    修改面向对象

--- docs/.vuepress/configs/sidebar/java.ts
+++ docs/.vuepress/configs/sidebar/java.ts
@@ -33,33 +33,33 @@ export const javaSidebar: SidebarConfigArray = [
         text: "面向对象",
         children: [
           "oop-introduce.md",
-          {
-            text: "类和对象",
-            children: [
-              "oop-fields-methods.md",
-              "oop-static.md",
-              "oop-constructor.md",
-              "oop-records.md",
-              "oop-package.md",
-            ],
-          },
-          {
-            text: "继承",
-            children: [
-              "oop-sub-super-classes.md",
-              "oop-abstract-classes.md",
-              "oop-enum-classes.md",
-              "oop-sealed-classes.md",
-            ],
-          },
-          {
-            text: "更多",
-            children: [
-              "oop-interface.md",
-              "oop-lambda-expressions.md",
-              "oop-inner-classes.md",
-            ],
-          },
+          // {
+          //   text: "类和对象",
+          //   children: [
+          //     "oop-fields-methods.md",
+          //     "oop-static.md",
+          //     "oop-constructor.md",
+          //     "oop-records.md",
+          //     "oop-package.md",
+          //   ],
+          // },
+          // {
+          //   text: "继承",
+          //   children: [
+          //     "oop-sub-super-classes.md",
+          //     "oop-abstract-classes.md",
+          //     "oop-enum-classes.md",
+          //     "oop-sealed-classes.md",
+          //   ],
+          // },
+          // {
+          //   text: "更多",
+          //   children: [
+          //     "oop-interface.md",
+          //     "oop-lambda-expressions.md",
+          //     "oop-inner-classes.md",
+          //   ],
+          // },
         ],
       },
       {
--- docs/java/oop-introduce.md
+++ docs/java/oop-introduce.md
@@ -1,5 +1,6 @@
 # 面向对象介绍
 
-:::tip
-待更新
-:::
+推荐阅读：
+
+- [Java 面向对象知识详解 —— 葛涛宁的博客](https://getaoning.com/archives/java-oop/)
+- [Java 函数式编程详解 —— 葛涛宁的博客](https://getaoning.com/archives/java-functional-program/)
