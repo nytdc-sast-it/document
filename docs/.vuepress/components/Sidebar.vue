965eda8428a08f92a9e7991a2220408e2841e676
Taoning Ge <gtn1024o@outlook.com>
2022-07-24 17:09:48 +0800

    修改ad

--- docs/.vuepress/components/Sidebar.vue
+++ docs/.vuepress/components/Sidebar.vue
@@ -1,12 +1,12 @@
 <template>
   <Sidebar>
-    <template #bottom>
+    <template #top>
       <div class="imgs">
         <a
           href="https://cloud.tencent.com/act/cps/redirect?redirect=2446&cps_key=0bf524a2bd69e68e98d9f0af95de4fbd&from=console"
           target="_blank"
         >
-          <img src="/assets/img/tencentcloud.svg" alt="tencent cloud" />
+          <img src="/assets/img/XtDpf92e.jpg" alt="tencent cloud" />
         </a>
       </div>
     </template>
@@ -17,15 +17,12 @@ import Sidebar from "@theme-hope/module/sidebar/components/Sidebar";
 </script>
 <style scoped>
 .imgs {
-  position: absolute;
-  bottom: 20px;
-  left: 50%;
-  transform: translateX(-50%);
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
 .imgs img {
-  width: 200px;
-  /* height: 80%; */
-  margin-right: 20px;
+  width: 100%;
 }
 </style>
